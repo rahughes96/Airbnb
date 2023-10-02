7c67484cac58e2047954082c54d776cda66060e7
rahughes96 <rahughes96@gmail.com>
2023-10-02 11:01:32 +0100

    adds functionality to tune model hyperparamaters

--- modelling.py
+++ modelling.py
@@ -1,6 +1,8 @@
 from unicodedata import normalize
 import pandas as pd
-import seaborn as sns
+import itertools
+import numpy as np
+#import seaborn as sns
 import matplotlib.pyplot as plt
 from tabular_data import load_airbnb
 from tabular_data import clean_tabular_data
@@ -11,8 +13,10 @@ from sklearn import datasets
 from sklearn.preprocessing import scale
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import SGDRegressor
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
 
 class MGS():
 
@@ -78,12 +82,83 @@ class MGS():
         plt.legend(loc='best', fancybox = True, shadow=True)
         plt.grid(True)
         plt.show()
+    
+    def _generate_hyperparameter_combinations(self, hyperparameters):
+        keys = list(hyperparameters.keys())
+        values = list(hyperparameters.values())
+        for combination in itertools.product(*values):
+            yield dict(zip(keys, combination))
+
+
+    def custom_tune_regression_model_hyperparameters(self, model_class, xtrain, ytrain, xvalid, yvalid, hyperparameters):
+        best_model = None
+        best_hyperparameters = {}
+        best_rmse = float('inf')
+
+        for param_combination in self._generate_hyperparameter_combinations(hyperparameters):
+            model = model_class(**param_combination)
+            model.fit(xtrain, ytrain)
+
+            ypred_valid = model.predict(xvalid)
+            rmse = np.sqrt(mean_squared_error(yvalid, ypred_valid))
+
+            if rmse < best_rmse:
+                best_rmse = rmse
+                best_model = model
+                best_hyperparameters = param_combination
+
+        performance_metrics = {
+            "validation_RMSE": best_rmse,
+            # You can add more performance metrics if needed
+        }
+
+        return best_model, best_hyperparameters, performance_metrics
+
+    def tune_regression_model_hyperparameters(self, model_class, param_grid, xtrain, ytrain, xvalid, yvalid):
+        grid_search = GridSearchCV(
+            estimator=model_class(),
+            param_grid=param_grid,
+            scoring='neg_mean_squared_error',  # Use 'neg_mean_squared_error' for RMSE
+            cv=5  # You can adjust the number of cross-validation folds as needed
+        )
+
+        grid_search.fit(xtrain, ytrain)
+
+        best_model = grid_search.best_estimator_
+        best_hyperparameters = grid_search.best_params_
+        validation_rmse = np.sqrt(-grid_search.best_score_)
+
+        return best_model, best_hyperparameters, validation_rmse
+
 
 if __name__ == "__main__":
+
     mgs = MGS()
     mgs.sgd('/Users/ryanhughes/Desktop/Aicore/Airbnb/Airbnb/AirbnbData/Raw_Data/tabular_data/listing.csv', 'Price_Night')
     
-    #print(mgs.train_split)
+hyperparameters = {
+    'alpha': [0.001, 0.01, 0.1, 1.0],
+    'penalty': ['l1', 'l2', 'elasticnet'],
+    'max_iter': [1000, 2000, 3000],
+}
+
+best_model, best_hyperparams, metrics = mgs.custom_tune_regression_model_hyperparameters(
+    model_class=SGDRegressor,
+    xtrain=mgs.train_split['xtrain'],
+    ytrain=mgs.train_split['ytrain'],
+    xvalid=mgs.train_split['xvalid'],
+    yvalid=mgs.train_split['yvalid'],
+    hyperparameters=hyperparameters
+)
+
+print("Best hyperparameters:", best_hyperparams)
+print("Validation RMSE:", metrics["validation_RMSE"])
+
+
+
+
+
+
 
 
 
