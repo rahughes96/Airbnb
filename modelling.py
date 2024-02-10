d4767397d18ba7ec0b980a00969d28d8578668e6
Ryan Hughes <ryanhughes@ryans-mbp.home>
2024-02-10 12:03:29 +0000

    evaluates the best model and returns its parameters and metrics

--- modelling.py
+++ modelling.py
@@ -1,68 +1,54 @@
-from unicodedata import normalize
 import pandas as pd
-import itertools
 import numpy as np
-#import seaborn as sns
-import matplotlib.pyplot as plt
-from tabular_data import load_airbnb
-from tabular_data import clean_tabular_data
-import os
 import joblib
 import json
-
+import os
+import itertools
+import matplotlib.pyplot as plt
 from sklearn import model_selection
-from sklearn import linear_model
-from sklearn import datasets
 from sklearn.preprocessing import scale
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import SGDRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from tabular_data import load_airbnb
+from tabular_data import clean_tabular_data
 
 class MGS():
-
     def __init__(self):
-        self.train_split ={}
+        self.train_split = {}
         self.best_model = None
         self.best_hyperparameters = {}
         self.metrics = {}
 
-    def sgd(self, path, label):
+    def sgd(self, path, label, plot = False):
         airbnb_data = pd.read_csv(path)
         cleaned_airbnb_data = clean_tabular_data(airbnb_data)
         cleaned_airbnb_data[['Cleanliness_rating','Accuracy_rating','Communication_rating','Location_rating','Check-in_rating','Value_rating','Description']] = cleaned_airbnb_data[['Cleanliness_rating','Accuracy_rating','Communication_rating','Location_rating','Check-in_rating','Value_rating','Description']].dropna()
         cleaned_airbnb_data.to_csv('/Users/ryanhughes/Desktop/Aicore/Airbnb/Airbnb/AirbnbData/Raw_Data/tabular_data/listing_test.csv')
+    
 
-        print("clean airbnb data:", cleaned_airbnb_data.head())
+        print("Cleaned Airbnb data:", cleaned_airbnb_data.head())
 
-        #Airbnb_data = pd.read_csv(path)
-
-        (X,y) = load_airbnb(cleaned_airbnb_data, label)
-        #print("X and y:")
-        #print(X,y)
-
-
-        #(X,y) = load_airbnb(Airbnb_data, label)
-
+        (X, y) = load_airbnb(cleaned_airbnb_data, label)
         X = scale(X)
         y = scale(y)
-        #print("scale(X) and scale(y):")
-        #print(X,y)
-        print(X)
-        xtrain, xtest, ytrain, ytest = model_selection.train_test_split(X, y, test_size=0.3)
 
+        xtrain, xtest, ytrain, ytest = model_selection.train_test_split(X, y, test_size=0.3)
         xvalid, xtest, yvalid, ytest = model_selection.train_test_split(xtest, ytest, test_size=0.5)
 
         self.train_split = {
-            'xtrain' : xtrain,
-            'xtest' : xtest,
-            'ytrain' : ytrain,
-            'ytest' : ytest,
-            'xvalid' :xvalid,
-            'yvalid' : yvalid
+            'xtrain': xtrain,
+            'xtest': xtest,
+            'ytrain': ytrain,
+            'ytest': ytest,
+            'xvalid': xvalid,
+            'yvalid': yvalid
         }
+
         sgdr = SGDRegressor()
         sgdr.fit(mgs.train_split['xtrain'], mgs.train_split['ytrain'])
 
@@ -79,22 +65,36 @@ class MGS():
         print("MSE: ", mse)
         print("RMSE: ", mse**(1/2.0))
 
+
+        if plot == True:
+            x_ax = range(len(ytest))
+            plt.plot(x_ax, ytest, linewidth=1, label="original")
+            plt.plot(x_ax, ypred, linewidth=1.1, label="predicted")
+            plt.title("y-test and y-predicted data")
+            plt.xlabel('X-axis')
+            plt.ylabel('Y-axis')
+            plt.legend(loc='best', fancybox = True, shadow=True)
+            plt.grid(True)
+            plt.show()
+    
+    def plot_predictions(self, model, xtest, ytest, title="y-test and y-predicted data"):
+        ypred = model.predict(xtest)
+
         x_ax = range(len(ytest))
         plt.plot(x_ax, ytest, linewidth=1, label="original")
         plt.plot(x_ax, ypred, linewidth=1.1, label="predicted")
-        plt.title("y-test and y-predicted data")
+        plt.title(title)
         plt.xlabel('X-axis')
         plt.ylabel('Y-axis')
-        plt.legend(loc='best', fancybox = True, shadow=True)
+        plt.legend(loc='best', fancybox=True, shadow=True)
         plt.grid(True)
         plt.show()
-    
+
     def _generate_hyperparameter_combinations(self, hyperparameters):
         keys = list(hyperparameters.keys())
         values = list(hyperparameters.values())
         for combination in itertools.product(*values):
             yield dict(zip(keys, combination))
-
 
     def custom_tune_regression_model_hyperparameters(self, model_class, xtrain, ytrain, xvalid, yvalid, hyperparameters):
         best_model = None
@@ -115,77 +115,238 @@ class MGS():
 
         performance_metrics = {
             "validation_RMSE": best_rmse,
-            # You can add more performance metrics if needed
         }
 
         return best_model, best_hyperparameters, performance_metrics
 
-    def tune_regression_model_hyperparameters(self, model_class, param_grid, xtrain, ytrain, xvalid, yvalid):
+    def tune_regression_model_hyperparameters(self, model_class, xtrain, ytrain, xvalid, yvalid, param_grid):
         grid_search = GridSearchCV(
             estimator=model_class(),
             param_grid=param_grid,
-            scoring='neg_mean_squared_error',  # Use 'neg_mean_squared_error' for RMSE
-            cv=5  # You can adjust the number of cross-validation folds as needed
+            scoring='neg_mean_squared_error',
+            cv=5
         )
 
         grid_search.fit(xtrain, ytrain)
 
-        best_model = grid_search.best_estimator_
-        best_hyperparameters = grid_search.best_params_
+        self.best_model = grid_search.best_estimator_
+        self.best_hyperparameters = grid_search.best_params_
         validation_rmse = np.sqrt(-grid_search.best_score_)
 
-        return best_model, best_hyperparameters, validation_rmse
+        self.metrics["validation_RMSE"] = validation_rmse      
 
-    def save_model(self, folder="models/regression/linear_regression"):
+        # Additional steps for using xvalid and yvalid if needed
+        ypred_valid = grid_search.predict(xvalid)
+        valid_rmse = np.sqrt(mean_squared_error(yvalid, ypred_valid))
+        self.metrics["validation_RMSE_on_xvalid"] = valid_rmse
+
+        performance_metrics = {
+            "validation_RMSE": valid_rmse
+        }  
+
+        return self.best_model, self.best_hyperparameters, performance_metrics
+
+    def save_model(self, folder, best_model=None, best_hyperparameters=None, metrics=None):
         # Create the folder if it doesn't exist
         os.makedirs(folder, exist_ok=True)
 
         # Save the trained model using joblib
         model_filename = os.path.join(folder, "model.joblib")
-        joblib.dump(self.best_model, model_filename)
+        joblib.dump(best_model, model_filename)
 
         # Save hyperparameters as JSON
         hyperparameters_filename = os.path.join(folder, "hyperparameters.json")
         with open(hyperparameters_filename, "w") as hyperparameters_file:
-            json.dump(self.best_hyperparameters, hyperparameters_file, indent=4)
+            json.dump(best_hyperparameters, hyperparameters_file, indent=4)
 
         # Save performance metrics as JSON
         metrics_filename = os.path.join(folder, "metrics.json")
         with open(metrics_filename, "w") as metrics_file:
-            json.dump(self.metrics, metrics_file, indent=4)
+            json.dump(metrics, metrics_file, indent=4)
 
-        
+    
+    def evaluate_all_models(self, path, label):
+            # Load and clean the data
+            print("Load and clean the data")
+            airbnb_data = pd.read_csv(path)
+            cleaned_airbnb_data = clean_tabular_data(airbnb_data)
+            cleaned_airbnb_data.to_csv('/Users/ryanhughes/Desktop/Aicore/Airbnb/Airbnb/AirbnbData/Raw_Data/tabular_data/listing_test.csv')
+
+            (X, y) = load_airbnb(cleaned_airbnb_data, label)
+
+            # Train-test split
+            print("Train-test split")
+            X = scale(X)
+            y = scale(y)
+            xtrain, xtest, ytrain, ytest = model_selection.train_test_split(X, y, test_size=0.3)
+            xvalid, xtest, yvalid, ytest = model_selection.train_test_split(xtest, ytest, test_size=0.5)
+
+            self.train_split = {
+                'xtrain': xtrain,
+                'xtest': xtest,
+                'ytrain': ytrain,
+                'ytest': ytest,
+                'xvalid': xvalid,
+                'yvalid': yvalid
+            }
+
+            # Evaluate Linear Regression
+            print("Evaluate Linear Regression")
+            sgd_hyperparameters={
+                    'alpha': [0.001, 0.01, 0.1, 1.0],
+                    'penalty': ['l1', 'l2', 'elasticnet'],
+                    'max_iter': [1000, 2000, 3000],
+            }
+            sgd_best_model, sgd_best_hyperparams, sgd_metrics = self.tune_regression_model_hyperparameters(
+                model_class=SGDRegressor,
+                xtrain=self.train_split['xtrain'],
+                ytrain=self.train_split['ytrain'],
+                xvalid=self.train_split['xvalid'],
+                yvalid=self.train_split['yvalid'],
+                param_grid=sgd_hyperparameters
+            )
+
+            self.save_model(folder="models/regression/decision_tree", best_model=sgd_best_model,
+                        best_hyperparameters=sgd_best_hyperparams, metrics=sgd_metrics)
+            xtest_sgd = self.train_split['xtest']
+            ytest_sgd = self.train_split['ytest']
+            self.plot_predictions(model=sgd_best_model, xtest=xtest_sgd, ytest=ytest_sgd, title="Linear Regression Predictions")
+
+            # Evaluate Decision Tree
+            print("Evaluate Decision Tree")
+            dt_hyperparameters = {
+                'max_depth': [None, 10, 20, 30],
+                'min_samples_split': [2, 5, 10],
+                'min_samples_leaf': [1, 2, 4]
+            }
+            dt_best_model, dt_best_hyperparams, dt_metrics = self.tune_regression_model_hyperparameters(
+                model_class=DecisionTreeRegressor,
+                xtrain=self.train_split['xtrain'],
+                ytrain=self.train_split['ytrain'],
+                xvalid=self.train_split['xvalid'],
+                yvalid=self.train_split['yvalid'],
+                param_grid=dt_hyperparameters
+            )
+            self.save_model(folder="models/regression/decision_tree", best_model=dt_best_model,
+                        best_hyperparameters=dt_best_hyperparams, metrics=dt_metrics)
+            
+            xtest_dt = self.train_split['xtest']
+            ytest_dt = self.train_split['ytest']
+            self.plot_predictions(model=dt_best_model, xtest=xtest_dt, ytest=ytest_dt, title="Decision Tree Predictions")
+
+            # Evaluate Random Forest
+            print("Evaluate Random Forest")
+            rf_hyperparameters = {
+                'n_estimators': [50, 100, 200],
+                'max_depth': [None, 10, 20, 30],
+                'min_samples_split': [2, 5, 10],
+                'min_samples_leaf': [1, 2, 4]
+            }
+            rf_best_model, rf_best_hyperparams, rf_metrics = self.tune_regression_model_hyperparameters(
+                model_class=RandomForestRegressor,
+                xtrain=self.train_split['xtrain'],
+                ytrain=self.train_split['ytrain'],
+                xvalid=self.train_split['xvalid'],
+                yvalid=self.train_split['yvalid'],
+                param_grid=rf_hyperparameters,
+            )
+            self.save_model(folder="models/regression/random_forest", best_model=rf_best_model,
+                        best_hyperparameters=rf_best_hyperparams, metrics=rf_metrics)
+            
+            xtest_rf = self.train_split['xtest']
+            ytest_rf = self.train_split['ytest']
+            self.plot_predictions(model=rf_best_model, xtest=xtest_rf, ytest=ytest_rf, title="Random Forest Predictions")
+
+
+            # Evaluate Gradient Boosting
+            print("Evaluate Gradient Boosting")
+            gb_hyperparameters = {
+                'n_estimators': [50, 100, 200],
+                'learning_rate': [0.01, 0.1, 0.2],
+                'max_depth': [3, 5, 7],
+                'min_samples_split': [2, 5, 10],
+                'min_samples_leaf': [1, 2, 4]
+            }
+            gb_best_model, gb_best_hyperparams, gb_metrics = self.custom_tune_regression_model_hyperparameters(
+                model_class=GradientBoostingRegressor,
+                xtrain=self.train_split['xtrain'],
+                ytrain=self.train_split['ytrain'],
+                xvalid=self.train_split['xvalid'],
+                yvalid=self.train_split['yvalid'],
+                hyperparameters=gb_hyperparameters
+            )
+            self.save_model(folder="models/regression/gradient_boosting", best_model=gb_best_model,
+                            best_hyperparameters=gb_best_hyperparams, metrics=gb_metrics)
+
+            xtest_gb = self.train_split['xtest']
+            ytest_gb = self.train_split['ytest']
+            self.plot_predictions(model=gb_best_model, xtest=xtest_gb, ytest=ytest_gb, title="Gradient Boosting Predictions")
+
+    def find_best_model(self):
+        # Define a list of models and their respective folders
+        models_folders = [
+            ("Linear Regression", "models/regression/linear_regression"),
+            ("Decision Tree", "models/regression/decision_tree"),
+            ("Random Forest", "models/regression/random_forest"),
+            ("Gradient Boosting", "models/regression/gradient_boosting")
+        ]
+
+        best_model_name = None
+        best_model = None
+        best_hyperparameters = {}
+        best_performance_metrics = {}
+        best_rmse = float('inf')
+
+        for model_name, folder in models_folders:
+            # Load the model
+            model_filename = os.path.join(folder, "model.joblib")
+            loaded_model = joblib.load(model_filename)
+
+            # Load hyperparameters
+            hyperparameters_filename = os.path.join(folder, "hyperparameters.json")
+            with open(hyperparameters_filename, "r") as hyperparameters_file:
+                hyperparameters = json.load(hyperparameters_file)
+
+            # Load performance metrics
+            metrics_filename = os.path.join(folder, "metrics.json")
+            with open(metrics_filename, "r") as metrics_file:
+                performance_metrics = json.load(metrics_file)
+
+            # Check if the current model has a lower RMSE
+            if performance_metrics["validation_RMSE"] < best_rmse:
+                best_rmse = performance_metrics["validation_RMSE"]
+                best_model_name = model_name
+                best_model = loaded_model
+                best_hyperparameters = hyperparameters
+                best_performance_metrics = performance_metrics
+
+        print("Best Model:", best_model_name)
+        print("Best Hyperparameters:", best_hyperparameters)
+        print("Best Performance Metrics:", best_performance_metrics)
+
+        return best_model, best_hyperparameters, best_performance_metrics
+
+
 if __name__ == "__main__":
-
     mgs = MGS()
-    mgs.sgd('/Users/ryanhughes/Desktop/Aicore/Airbnb/Airbnb/AirbnbData/Raw_Data/tabular_data/listing.csv', 'Price_Night')
 
-    hyperparameters = {
+    #print("Load and preprocess data")
+    #mgs.sgd('/Users/ryanhughes/Desktop/Aicore/Airbnb/Airbnb/AirbnbData/Raw_Data/tabular_data/listing.csv', 'Price_Night', plot=True)
+
+    #print("Define hyperparameter grid")
+    param_grid = {
         'alpha': [0.001, 0.01, 0.1, 1.0],
         'penalty': ['l1', 'l2', 'elasticnet'],
         'max_iter': [1000, 2000, 3000],
     }
 
-    best_model, best_hyperparams, metrics = mgs.custom_tune_regression_model_hyperparameters(
-        model_class=SGDRegressor,
-        xtrain=mgs.train_split['xtrain'],
-        ytrain=mgs.train_split['ytrain'],
-        xvalid=mgs.train_split['xvalid'],
-        yvalid=mgs.train_split['yvalid'],
-        hyperparameters=hyperparameters
-    )
-    best_model = best_model
-    print("Best hyperparameters:", best_hyperparams)
-    print("Validation RMSE:", metrics["validation_RMSE"])
+    #print("Tune hyperparameters")
+    #mgs.tune_regression_model_hyperparameters(model_class=SGDRegressor, param_grid=param_grid)
 
-    mgs.save_model(folder="models/regression/linear_regression")
+    #print("Save the model, hyperparameters, and metrics")
+    #mgs.save_model(folder="models/regression/linear_regression")
 
+    print("Evaluate all models")
+    mgs.evaluate_all_models('/Users/ryanhughes/Desktop/Aicore/Airbnb/Airbnb/AirbnbData/Raw_Data/tabular_data/listing.csv', 'Price_Night')
 
-
-
-
-
-
-
-
-
+    best_model, best_hyperparameters, best_performance_metrics = mgs.find_best_model()
