4f588ace794c24ccb4089a40b20377293a5c6414
Ryan Hughes <ryanhughes@ryans-mbp.home>
2024-02-12 15:45:27 +0000

    adds docstrings to modelling.py

--- modelling.py
+++ modelling.py
@@ -25,6 +25,18 @@ class MGS():
         self.metrics = {}
 
     def sgd(self, path, label, plot = False):
+
+        """
+
+        Perform Stochastic Gradient Descent regression on Airbnb data.
+
+        Attributes:
+            Path (str): The csv file where the raw data exists
+            label (str): The title of the column in which you want to predict
+            plot (bool): boolean value for whether you want to see the charts depicting your original and predicted data
+        
+        """
+
         airbnb_data = pd.read_csv(path)
         cleaned_airbnb_data = clean_tabular_data(airbnb_data)
         cleaned_airbnb_data[['Cleanliness_rating','Accuracy_rating','Communication_rating','Location_rating','Check-in_rating','Value_rating','Description']] = cleaned_airbnb_data[['Cleanliness_rating','Accuracy_rating','Communication_rating','Location_rating','Check-in_rating','Value_rating','Description']].dropna()
@@ -78,6 +90,19 @@ class MGS():
             plt.show()
     
     def plot_predictions(self, model, xtest, ytest, title="y-test and y-predicted data"):
+
+        """
+        
+        Plots the original and predicted values of the target variable for a given model.
+
+        Attributes:
+            model: The trained regression model used for prediction.
+            xtest (array-like): The features of the testing set.
+            ytest (array-like): The true labels of the testing set.
+            title (str, optional): The title of the plot (default is "y-test and y-predicted data")
+
+        """
+
         ypred = model.predict(xtest)
 
         x_ax = range(len(ytest))
@@ -91,12 +116,39 @@ class MGS():
         plt.show()
 
     def _generate_hyperparameter_combinations(self, hyperparameters):
+
+        """
+        Generates combinations of hyperparameter values for hyperparameter tuning.
+
+        Attributes:
+            hyperparameters (dict): A dictionary where keys are hyperparameter names and values are lists of possible values.
+        
+        """
+
         keys = list(hyperparameters.keys())
         values = list(hyperparameters.values())
         for combination in itertools.product(*values):
             yield dict(zip(keys, combination))
 
     def custom_tune_regression_model_hyperparameters(self, model_class, xtrain, ytrain, xvalid, yvalid, hyperparameters):
+
+        """
+
+        Performs hyperparameter tuning from scratch for a regression model using custom combinations.
+
+        Parameters:
+            model_class (class): The class of the regression model to be tuned.
+            xtrain (array-like): The features of the training set.
+            ytrain (array-like): The true labels of the training set.
+            xvalid (array-like): The features of the validation set.
+            yvalid (array-like): The true labels of the validation set.
+            hyperparameters (dict): A dictionary where keys are hyperparameter names and values are lists of possible values.
+
+        Returns:
+            tuple: A tuple containing the best trained model, its corresponding hyperparameters, and performance metrics.
+
+        """
+
         best_model = None
         best_hyperparameters = {}
         best_rmse = float('inf')
@@ -120,6 +172,24 @@ class MGS():
         return best_model, best_hyperparameters, performance_metrics
 
     def tune_regression_model_hyperparameters(self, model_class, xtrain, ytrain, xvalid, yvalid, param_grid):
+
+        """
+
+        Performs hyperparameter tuning for a regression model using grid search.
+
+        Parameters:
+            model_class (class): The class of the regression model to be tuned.
+            xtrain (array-like): The features of the training set.
+            ytrain (array-like): The true labels of the training set.
+            xvalid (array-like): The features of the validation set.
+            yvalid (array-like): The true labels of the validation set.
+            param_grid (dict): A dictionary specifying hyperparameters and their search spaces.
+
+        Returns:
+            tuple: A tuple containing the best trained model, its corresponding hyperparameters, and performance metrics
+
+        """
+
         grid_search = GridSearchCV(
             estimator=model_class(),
             param_grid=param_grid,
@@ -147,6 +217,19 @@ class MGS():
         return self.best_model, self.best_hyperparameters, performance_metrics
 
     def save_model(self, folder, best_model=None, best_hyperparameters=None, metrics=None):
+
+        """
+
+        Saves the trained regression model, hyperparameters, and performance metrics to a specified folder.
+
+        Parameters:
+            folder (str): The path to the folder where the model, hyperparameters, and metrics will be saved.
+            best_model: The trained regression model to be saved (default is None).
+            best_hyperparameters (dict): The hyperparameters used for training the model (default is None).
+            metrics (dict): The performance metrics of the model (default is None).
+
+        """
+
         # Create the folder if it doesn't exist
         os.makedirs(folder, exist_ok=True)
 
@@ -166,123 +249,144 @@ class MGS():
 
     
     def evaluate_all_models(self, path, label):
+
+        """
+
+        Load, clean, and evaluate multiple regression models on Airbnb data.
+
+        Parameters:
+            path (str): The path to the CSV file containing Airbnb data.
+            label (str): The title of the column to predict.
+
+        """
+
             # Load and clean the data
-            print("Load and clean the data")
-            airbnb_data = pd.read_csv(path)
-            cleaned_airbnb_data = clean_tabular_data(airbnb_data)
-            cleaned_airbnb_data.to_csv('/Users/ryanhughes/Desktop/Aicore/Airbnb/Airbnb/AirbnbData/Raw_Data/tabular_data/listing_test.csv')
+        print("Load and clean the data")
+        airbnb_data = pd.read_csv(path)
+        cleaned_airbnb_data = clean_tabular_data(airbnb_data)
+        cleaned_airbnb_data.to_csv('/Users/ryanhughes/Desktop/Aicore/Airbnb/Airbnb/AirbnbData/Raw_Data/tabular_data/listing_test.csv')
 
-            (X, y) = load_airbnb(cleaned_airbnb_data, label)
+        (X, y) = load_airbnb(cleaned_airbnb_data, label)
 
-            # Train-test split
-            print("Train-test split")
-            X = scale(X)
-            y = scale(y)
-            xtrain, xtest, ytrain, ytest = model_selection.train_test_split(X, y, test_size=0.3)
-            xvalid, xtest, yvalid, ytest = model_selection.train_test_split(xtest, ytest, test_size=0.5)
+        # Train-test split
+        print("Train-test split")
+        X = scale(X)
+        y = scale(y)
+        xtrain, xtest, ytrain, ytest = model_selection.train_test_split(X, y, test_size=0.3)
+        xvalid, xtest, yvalid, ytest = model_selection.train_test_split(xtest, ytest, test_size=0.5)
 
-            self.train_split = {
-                'xtrain': xtrain,
-                'xtest': xtest,
-                'ytrain': ytrain,
-                'ytest': ytest,
-                'xvalid': xvalid,
-                'yvalid': yvalid
-            }
+        self.train_split = {
+            'xtrain': xtrain,
+            'xtest': xtest,
+            'ytrain': ytrain,
+            'ytest': ytest,
+            'xvalid': xvalid,
+            'yvalid': yvalid
+        }
 
-            # Evaluate Linear Regression
-            print("Evaluate Linear Regression")
-            sgd_hyperparameters={
-                    'alpha': [0.001, 0.01, 0.1, 1.0],
-                    'penalty': ['l1', 'l2', 'elasticnet'],
-                    'max_iter': [1000, 2000, 3000],
-            }
-            sgd_best_model, sgd_best_hyperparams, sgd_metrics = self.tune_regression_model_hyperparameters(
-                model_class=SGDRegressor,
-                xtrain=self.train_split['xtrain'],
-                ytrain=self.train_split['ytrain'],
-                xvalid=self.train_split['xvalid'],
-                yvalid=self.train_split['yvalid'],
-                param_grid=sgd_hyperparameters
-            )
+        # Evaluate Linear Regression
+        print("Evaluate Linear Regression")
+        sgd_hyperparameters={
+                'alpha': [0.001, 0.01, 0.1, 1.0],
+                'penalty': ['l1', 'l2', 'elasticnet'],
+                'max_iter': [1000, 2000, 3000],
+        }
+        sgd_best_model, sgd_best_hyperparams, sgd_metrics = self.tune_regression_model_hyperparameters(
+            model_class=SGDRegressor,
+            xtrain=self.train_split['xtrain'],
+            ytrain=self.train_split['ytrain'],
+            xvalid=self.train_split['xvalid'],
+            yvalid=self.train_split['yvalid'],
+            param_grid=sgd_hyperparameters
+        )
 
-            self.save_model(folder="models/regression/decision_tree", best_model=sgd_best_model,
-                        best_hyperparameters=sgd_best_hyperparams, metrics=sgd_metrics)
-            xtest_sgd = self.train_split['xtest']
-            ytest_sgd = self.train_split['ytest']
-            self.plot_predictions(model=sgd_best_model, xtest=xtest_sgd, ytest=ytest_sgd, title="Linear Regression Predictions")
+        self.save_model(folder="models/regression/decision_tree", best_model=sgd_best_model,
+                    best_hyperparameters=sgd_best_hyperparams, metrics=sgd_metrics)
+        xtest_sgd = self.train_split['xtest']
+        ytest_sgd = self.train_split['ytest']
+        self.plot_predictions(model=sgd_best_model, xtest=xtest_sgd, ytest=ytest_sgd, title="Linear Regression Predictions")
 
-            # Evaluate Decision Tree
-            print("Evaluate Decision Tree")
-            dt_hyperparameters = {
-                'max_depth': [None, 10, 20, 30],
-                'min_samples_split': [2, 5, 10],
-                'min_samples_leaf': [1, 2, 4]
-            }
-            dt_best_model, dt_best_hyperparams, dt_metrics = self.tune_regression_model_hyperparameters(
-                model_class=DecisionTreeRegressor,
-                xtrain=self.train_split['xtrain'],
-                ytrain=self.train_split['ytrain'],
-                xvalid=self.train_split['xvalid'],
-                yvalid=self.train_split['yvalid'],
-                param_grid=dt_hyperparameters
-            )
-            self.save_model(folder="models/regression/decision_tree", best_model=dt_best_model,
-                        best_hyperparameters=dt_best_hyperparams, metrics=dt_metrics)
-            
-            xtest_dt = self.train_split['xtest']
-            ytest_dt = self.train_split['ytest']
-            self.plot_predictions(model=dt_best_model, xtest=xtest_dt, ytest=ytest_dt, title="Decision Tree Predictions")
+        # Evaluate Decision Tree
+        print("Evaluate Decision Tree")
+        dt_hyperparameters = {
+            'max_depth': [None, 10, 20, 30],
+            'min_samples_split': [2, 5, 10],
+            'min_samples_leaf': [1, 2, 4]
+        }
+        dt_best_model, dt_best_hyperparams, dt_metrics = self.tune_regression_model_hyperparameters(
+            model_class=DecisionTreeRegressor,
+            xtrain=self.train_split['xtrain'],
+            ytrain=self.train_split['ytrain'],
+            xvalid=self.train_split['xvalid'],
+            yvalid=self.train_split['yvalid'],
+            param_grid=dt_hyperparameters
+        )
+        self.save_model(folder="models/regression/decision_tree", best_model=dt_best_model,
+                    best_hyperparameters=dt_best_hyperparams, metrics=dt_metrics)
+        
+        xtest_dt = self.train_split['xtest']
+        ytest_dt = self.train_split['ytest']
+        self.plot_predictions(model=dt_best_model, xtest=xtest_dt, ytest=ytest_dt, title="Decision Tree Predictions")
 
-            # Evaluate Random Forest
-            print("Evaluate Random Forest")
-            rf_hyperparameters = {
-                'n_estimators': [50, 100, 200],
-                'max_depth': [None, 10, 20, 30],
-                'min_samples_split': [2, 5, 10],
-                'min_samples_leaf': [1, 2, 4]
-            }
-            rf_best_model, rf_best_hyperparams, rf_metrics = self.tune_regression_model_hyperparameters(
-                model_class=RandomForestRegressor,
-                xtrain=self.train_split['xtrain'],
-                ytrain=self.train_split['ytrain'],
-                xvalid=self.train_split['xvalid'],
-                yvalid=self.train_split['yvalid'],
-                param_grid=rf_hyperparameters,
-            )
-            self.save_model(folder="models/regression/random_forest", best_model=rf_best_model,
-                        best_hyperparameters=rf_best_hyperparams, metrics=rf_metrics)
-            
-            xtest_rf = self.train_split['xtest']
-            ytest_rf = self.train_split['ytest']
-            self.plot_predictions(model=rf_best_model, xtest=xtest_rf, ytest=ytest_rf, title="Random Forest Predictions")
+        # Evaluate Random Forest
+        print("Evaluate Random Forest")
+        rf_hyperparameters = {
+            'n_estimators': [50, 100, 200],
+            'max_depth': [None, 10, 20, 30],
+            'min_samples_split': [2, 5, 10],
+            'min_samples_leaf': [1, 2, 4]
+        }
+        rf_best_model, rf_best_hyperparams, rf_metrics = self.tune_regression_model_hyperparameters(
+            model_class=RandomForestRegressor,
+            xtrain=self.train_split['xtrain'],
+            ytrain=self.train_split['ytrain'],
+            xvalid=self.train_split['xvalid'],
+            yvalid=self.train_split['yvalid'],
+            param_grid=rf_hyperparameters,
+        )
+        self.save_model(folder="models/regression/random_forest", best_model=rf_best_model,
+                    best_hyperparameters=rf_best_hyperparams, metrics=rf_metrics)
+        
+        xtest_rf = self.train_split['xtest']
+        ytest_rf = self.train_split['ytest']
+        self.plot_predictions(model=rf_best_model, xtest=xtest_rf, ytest=ytest_rf, title="Random Forest Predictions")
 
 
-            # Evaluate Gradient Boosting
-            print("Evaluate Gradient Boosting")
-            gb_hyperparameters = {
-                'n_estimators': [50, 100, 200],
-                'learning_rate': [0.01, 0.1, 0.2],
-                'max_depth': [3, 5, 7],
-                'min_samples_split': [2, 5, 10],
-                'min_samples_leaf': [1, 2, 4]
-            }
-            gb_best_model, gb_best_hyperparams, gb_metrics = self.custom_tune_regression_model_hyperparameters(
-                model_class=GradientBoostingRegressor,
-                xtrain=self.train_split['xtrain'],
-                ytrain=self.train_split['ytrain'],
-                xvalid=self.train_split['xvalid'],
-                yvalid=self.train_split['yvalid'],
-                hyperparameters=gb_hyperparameters
-            )
-            self.save_model(folder="models/regression/gradient_boosting", best_model=gb_best_model,
-                            best_hyperparameters=gb_best_hyperparams, metrics=gb_metrics)
+        # Evaluate Gradient Boosting
+        print("Evaluate Gradient Boosting")
+        gb_hyperparameters = {
+            'n_estimators': [50, 100, 200],
+            'learning_rate': [0.01, 0.1, 0.2],
+            'max_depth': [3, 5, 7],
+            'min_samples_split': [2, 5, 10],
+            'min_samples_leaf': [1, 2, 4]
+        }
+        gb_best_model, gb_best_hyperparams, gb_metrics = self.custom_tune_regression_model_hyperparameters(
+            model_class=GradientBoostingRegressor,
+            xtrain=self.train_split['xtrain'],
+            ytrain=self.train_split['ytrain'],
+            xvalid=self.train_split['xvalid'],
+            yvalid=self.train_split['yvalid'],
+            hyperparameters=gb_hyperparameters
+        )
+        self.save_model(folder="models/regression/gradient_boosting", best_model=gb_best_model,
+                        best_hyperparameters=gb_best_hyperparams, metrics=gb_metrics)
 
-            xtest_gb = self.train_split['xtest']
-            ytest_gb = self.train_split['ytest']
-            self.plot_predictions(model=gb_best_model, xtest=xtest_gb, ytest=ytest_gb, title="Gradient Boosting Predictions")
+        xtest_gb = self.train_split['xtest']
+        ytest_gb = self.train_split['ytest']
+        self.plot_predictions(model=gb_best_model, xtest=xtest_gb, ytest=ytest_gb, title="Gradient Boosting Predictions")
 
     def find_best_model(self):
+
+        """
+
+        Finds the best-performing regression model among a predefined list of models.
+
+        Returns:
+            tuple: A tuple containing the best model, its hyperparameters, and performance metrics.
+
+        """
+
         # Define a list of models and their respective folders
         models_folders = [
             ("Linear Regression", "models/regression/linear_regression"),
@@ -329,22 +433,6 @@ class MGS():
 
 if __name__ == "__main__":
     mgs = MGS()
-
-    #print("Load and preprocess data")
-    #mgs.sgd('/Users/ryanhughes/Desktop/Aicore/Airbnb/Airbnb/AirbnbData/Raw_Data/tabular_data/listing.csv', 'Price_Night', plot=True)
-
-    #print("Define hyperparameter grid")
-    param_grid = {
-        'alpha': [0.001, 0.01, 0.1, 1.0],
-        'penalty': ['l1', 'l2', 'elasticnet'],
-        'max_iter': [1000, 2000, 3000],
-    }
-
-    #print("Tune hyperparameters")
-    #mgs.tune_regression_model_hyperparameters(model_class=SGDRegressor, param_grid=param_grid)
-
-    #print("Save the model, hyperparameters, and metrics")
-    #mgs.save_model(folder="models/regression/linear_regression")
 
     print("Evaluate all models")
     mgs.evaluate_all_models('/Users/ryanhughes/Desktop/Aicore/Airbnb/Airbnb/AirbnbData/Raw_Data/tabular_data/listing.csv', 'Price_Night')
