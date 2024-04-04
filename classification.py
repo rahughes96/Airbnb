779f0e014ae5394bc53d7f98036958fb2025951c
Ryan Hughes <ryanhughes@Ryans-MBP.Home>
2024-04-04 16:31:36 +0100

    fixes issues in listings.csv

--- classification.py
+++ classification.py
@@ -10,23 +10,17 @@ from sklearn.metrics import accuracy_score, classification_report
 import re
 
 class AirbnbLogisticRegression:
-    def __init__(self, filepath):
-        self.filepath = filepath
+    def __init__(self):
         self.data = None
         self.model = None
-        self.X_train = None
-        self.X_test = None
-        self.y_train = None
-        self.y_test = None
-        self.X_valid = None
-        self.y_valid = None
         self.best_hyperparameters = None
         self.best_model = None
         self.metrics = {}
+        self.train_split = {}
 
-    def load_data(self):
+    def load_data(self, filepath):
         # Load data
-        dataframe = pd.read_csv(self.filepath)
+        dataframe = pd.read_csv(filepath)
 
         # Preprocess data
         columns_to_drop = ['Unnamed: 0', 'ID', 'url']
@@ -44,8 +38,14 @@ class AirbnbLogisticRegression:
         #print('1st')
         #print(dataframe)
 
-        dataframe = dataframe.select_dtypes(['float64', 'int64'])
+        columns_to_filter = ["guests", "bedrooms"]
+        for column in columns_to_filter:
+            dataframe = dataframe[pd.to_numeric(dataframe[column], errors='coerce').notnull()]
+            dataframe[column] = dataframe[column].astype(int)
 
+        print("dataframe before: ", dataframe.info())
+        #dataframe = dataframe.select_dtypes(['float64', 'int64'])
+        #print("dataframe after: ", dataframe.info())
         dataframe[['Cleanliness_rating','Accuracy_rating','Communication_rating','Location_rating','Check-in_rating','Value_rating','Description']] = dataframe[['Cleanliness_rating','Accuracy_rating','Communication_rating','Location_rating','Check-in_rating','Value_rating','Description']].dropna(axis=1)
         #print("2nd")
         #print(dataframe)
@@ -56,14 +56,21 @@ class AirbnbLogisticRegression:
         # Split data into features and target variable
         X = dataframe.drop(columns=['Category'])
         y = dataframe['Category']
-        #print("TITS")
         #print(train_test_split(X, y, test_size=0.2, random_state=42))
 
         # Split data into train and test sets
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        self.X_train, self.X_valid, self.y_train, self.y_valid = train_test_split(self.X_train, self.y_train, test_size=0.25, random_state=42)
-        #print("TITS")
+        x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+        x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size=0.5, random_state=42)
         #print(self.y_train)
+
+        self.train_split = {
+            'xtrain': x_train,
+            'xtest': x_test,
+            'ytrain': y_train,
+            'ytest': y_test,
+            'xvalid': x_valid,
+            'yvalid': y_valid
+        }
 
         self.data = dataframe
 
@@ -72,46 +79,47 @@ class AirbnbLogisticRegression:
     def train_model(self):
         #print(self.X_train)
         # Train logistic regression model
-        self.model = LogisticRegression(max_iter=1000)
-        self.model.fit(self.X_train, self.y_train)
+        self.model = LogisticRegression(max_iter=5000)
+        self.model.fit(self.train_split["xtrain"], self.train_split["ytrain"])
 
     def preprocess_data(self):
         scaler = StandardScaler()
 
-        self.X_train_scaled = scaler.fit_transform(self.X_train)
-        self.X_test_scaled = scaler.transform(self.X_test)
+        self.X_train = scaler.fit_transform(self.train_split['xtrain'])
+        self.X_test = scaler.transform(self.train_split['xtest'])
+        self.X_valid = scaler.transform(self.train_split['xvalid'])
     
     def evaluate_model(self):
         # Predictions on training set
-        train_predictions = self.model.predict(self.X_train_scaled)
+        train_predictions = self.model.predict(self.train_split["xtrain"])
 
         # Predictions on test set
-        test_predictions = self.model.predict(self.X_test_scaled)
+        test_predictions = self.model.predict(self.train_split["xtest"])
 
         # Compute accuracy for training set
-        train_accuracy = accuracy_score(self.y_train, train_predictions)
+        train_accuracy = accuracy_score(self.train_split["ytrain"], train_predictions)
 
         # Compute accuracy for test set
-        test_accuracy = accuracy_score(self.y_test, test_predictions)
+        test_accuracy = accuracy_score(self.train_split["ytest"], test_predictions)
 
         # Compute precision for training set
-        train_precision = precision_score(self.y_train, train_predictions, average='weighted')
+        train_precision = precision_score(self.train_split["ytrain"], train_predictions, average='weighted')
 
         # Compute precision for test set
-        test_precision = precision_score(self.y_test, test_predictions, average='weighted')
+        test_precision = precision_score(self.train_split["ytest"], test_predictions, average='weighted')
 
         # Compute recall for training set
-        train_recall = recall_score(self.y_train, train_predictions, average='weighted')
+        train_recall = recall_score(self.train_split["ytrain"], train_predictions, average='weighted')
 
         # Compute recall for test set
-        test_recall = recall_score(self.y_test, test_predictions, average='weighted')
+        test_recall = recall_score(self.train_split["ytest"], test_predictions, average='weighted')
 
         # Compute F1 score for training set
-        train_f1_score = f1_score(self.y_train, train_predictions, average='weighted')
+        train_f1_score = f1_score(self.train_split["ytrain"], train_predictions, average='weighted')
 
         # Compute F1 score for test set
-        test_f1_score = f1_score(self.y_test, test_predictions, average='weighted')
-        """""
+        test_f1_score = f1_score(self.train_split["ytest"], test_predictions, average='weighted')
+        
         # Print the performance measures
         print("Training Set Metrics:")
         print("Accuracy:", train_accuracy)
@@ -122,39 +130,9 @@ class AirbnbLogisticRegression:
         print("Accuracy:", test_accuracy)
         print("Precision:", test_precision)
         print("Recall:", test_recall)
-        print("F1 Score:", test_f1_score)
-
-        """""
-
-    def tune_classification_model_hyperparameters(model_class, xtrain, ytrain, xvalid, yvalid, param_grid):
-        grid_search = GridSearchCV(
-            estimator=model_class(),
-            param_grid=param_grid,
-            scoring='accuracy',  # Use accuracy as the scoring metric
-            cv=5
-        )
-
-        grid_search.fit(xtrain, ytrain)
-
-        best_model = grid_search.best_estimator_
-        best_hyperparameters = grid_search.best_params_
-
-        # Calculate validation accuracy
-        validation_accuracy = accuracy_score(yvalid, best_model.predict(xvalid))
-
-        # Additional steps for performance evaluation if needed
-        ypred_valid = grid_search.predict(xvalid)
-        validation_classification_report = classification_report(yvalid, ypred_valid, output_dict=True)
-
-        performance_metrics = {
-            "validation_accuracy": validation_accuracy,
-            "validation_classification_report": validation_classification_report,
-            # Add other metrics if needed
-        }
-
-        return best_model, best_hyperparameters, performance_metrics
+        print("F1 Score:", test_f1_score)     
     
-    def tune_classification_model_hyperparameters_1(self, model_class, xtrain, ytrain, xvalid, yvalid, param_grid):
+    def tune_classification_model_hyperparameters(self, model_class, xtrain, ytrain, xvalid, yvalid, param_grid):
         grid_search = GridSearchCV(
             estimator=model_class(),
             param_grid=param_grid,
@@ -189,12 +167,12 @@ class AirbnbLogisticRegression:
 
 if __name__ == "__main__":
     filepath = 'AirbnbData/Raw_Data/tabular_data/listing.csv'
-    airbnb_lr = AirbnbLogisticRegression(filepath)
+    airbnb_lr = AirbnbLogisticRegression()
     print("load data...")
-    airbnb_lr.load_data()
+    airbnb_lr.load_data(filepath)
     #print(data)
     print("preprocess data...")
-    airbnb_lr.preprocess_data()
+    #airbnb_lr.preprocess_data()
     print("train model...")
     airbnb_lr.train_model()
     print("evaluate model...")
@@ -205,7 +183,9 @@ if __name__ == "__main__":
         'penalty': ['l1', 'l2'],
         'solver': ['liblinear', 'saga']
     }
-    print("perfomance metrics:")
-    best_model, best_hyperparameters, performance_metrics = airbnb_lr.tune_classification_model_hyperparameters_1(LogisticRegression, airbnb_lr.X_train, airbnb_lr.y_train, airbnb_lr.X_valid, airbnb_lr.y_valid, param_grid)
+    print("/n final perfomance metrics:")
+    best_model, best_hyperparameters, performance_metrics = airbnb_lr.tune_classification_model_hyperparameters(LogisticRegression, airbnb_lr.train_split["xtrain"], airbnb_lr.train_split["ytrain"], airbnb_lr.train_split["xvalid"], airbnb_lr.train_split["yvalid"], param_grid)
 
+    print(best_hyperparameters)
+    print(best_model)
 
